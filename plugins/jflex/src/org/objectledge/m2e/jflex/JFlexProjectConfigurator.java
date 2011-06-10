d20b01aa5c766605c0c1416a70ca87a10199ef2f
rkrzewski <rafal.krzewski@objectledge.org>
2011-06-10 02:06:48 +0200

    JFlex plugin configurator

---
+++ plugins/jflex/src/org/objectledge/m2e/jflex/JFlexProjectConfigurator.java
@@ -0,0 +1,16 @@
+package org.objectledge.m2e.jflex;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+
+public class JFlexProjectConfigurator extends AbstractJavaProjectConfigurator {
+	@Override
+	public AbstractBuildParticipant getBuildParticipant(
+			IMavenProjectFacade projectFacade, MojoExecution execution,
+			IPluginExecutionMetadata executionMetadata) {
+		return new JFlexBuildParticipant(execution);
+	}
+}
