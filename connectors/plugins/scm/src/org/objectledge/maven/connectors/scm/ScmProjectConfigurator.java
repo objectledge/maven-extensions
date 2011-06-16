2f057b27ed49dc85f9150e73519bfdabdd3bf914
rkrzewski <rafal.krzewski@gmail.com>
2011-06-16 03:30:21 +0200

    Inherit from AbstractProjectConfigurator

--- connectors/plugins/scm/src/org/objectledge/maven/connectors/scm/ScmProjectConfigurator.java
+++ connectors/plugins/scm/src/org/objectledge/maven/connectors/scm/ScmProjectConfigurator.java
@@ -1,12 +1,22 @@
 package org.objectledge.maven.connectors.scm;
 
 import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
-import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 
-public class ScmProjectConfigurator extends AbstractJavaProjectConfigurator {
+public class ScmProjectConfigurator extends AbstractProjectConfigurator {
+
+	@Override
+	public void configure(ProjectConfigurationRequest request,
+			IProgressMonitor monitor) throws CoreException {
+		// nothing to do
+	}
+	
 	@Override
 	public AbstractBuildParticipant getBuildParticipant(
 			IMavenProjectFacade projectFacade, MojoExecution execution,
