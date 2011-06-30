f71d4748b7d3764e80482657a51793e5334aa29c
rkrzewski <rafal.krzewski@gmail.com>
2011-06-30 03:30:06 +0200

    TOOLS-7: test plugin for JavaCC connector (work in progress)

---
+++ connectors/modules/javacc/tests/projects/javacc-01/src/test/java/org/objectledge/maven/connectors/javacc/tests/SampleParserTest.java
@@ -0,0 +1,28 @@
+package org.objectledge.maven.connectors.javacc.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class SampleParserTest {
+	
+	@Test
+	public void parseTest() throws ParseException {		
+		List<Integer> sequence = SampleParser.parse("1, 2, 3");
+		assertEquals(3, sequence.size());
+		assertEquals(1, sequence.get(0).intValue());
+	}
+	
+	@Test
+	public void parseExceptionTest() {
+		try {
+			SampleParser.parse("1, 2,,");
+			fail("should throw ParseException");
+		} catch (ParseException e) {
+			// OK
+		}
+	}
+}
---
+++ connectors/modules/javacc/tests/src/org/objectledge/maven/connectors/javacc/JavaCCGenerationTest.java
@@ -0,0 +1,33 @@
+package org.objectledge.maven.connectors.javacc;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.m2e.core.project.ResolverConfiguration;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+
+public class JavaCCGenerationTest extends AbstractMavenProjectTestCase {
+	public void test_01() throws Exception {
+		ResolverConfiguration configuration = new ResolverConfiguration();
+        IProject project1 = importProject( "projects/javacc-01/pom.xml", configuration );
+        waitForJobsToComplete();
+        
+        project1.build( IncrementalProjectBuilder.FULL_BUILD, monitor );
+        project1.build( IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor );
+        waitForJobsToComplete();
+
+        assertNoErrors( project1 );
+        
+        IJavaProject javaProject1 = JavaCore.create( project1 );
+        IClasspathEntry[] cp1 = javaProject1.getRawClasspath();
+
+        assertEquals( new Path( "/javacc-01/target/generated-sources/javacc" ), cp1[3].getPath() );
+
+        assertTrue( project1.getFile( "target/generated-sources/javacc/org/objectledge/maven/connectors/javacc/tests/SampleParser.java" ).isSynchronized( IResource.DEPTH_ZERO ) );
+        assertTrue( project1.getFile( "target/generated-sources/javacc/org/objectledge/maven/connectors/javacc/tests/SampleParser.java" ).isAccessible() );
+	}
+}
