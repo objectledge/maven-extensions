57c1fe630a2d59cab240f3e4e2cfb40729c998e8
rkrzewski <rafal.krzewski@gmail.com>
2011-07-05 00:48:05 +0200

    TOOLS-34: incremental initial-full and incremental builds separated to distinct projects

--- connectors/modules/coral/tests/src/org/objectledge/maven/connectors/coral/ProjectTest.java
+++ connectors/modules/coral/tests/src/org/objectledge/maven/connectors/coral/ProjectTest.java
@@ -1,20 +1,85 @@
 package org.objectledge.maven.connectors.coral;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.codehaus.plexus.util.FileUtils;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.m2e.core.project.ResolverConfiguration;
 import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
 
 public class ProjectTest extends AbstractMavenProjectTestCase {
-	public void test_01() throws Exception {
-		ResolverConfiguration configuration = new ResolverConfiguration();
-        IProject project1 = importProject( "projects/testproject-01/pom.xml", configuration );
-        waitForJobsToComplete();
-        
-        project1.build( IncrementalProjectBuilder.FULL_BUILD, monitor );
-        project1.build( IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor );
-        waitForJobsToComplete();
 
-        assertNoErrors( project1 );
+	public void testInitialBuild() throws Exception {
+		ResolverConfiguration configuration = new ResolverConfiguration();
+		IProject project1 = importProject("projects/testproject-01/pom.xml",
+				configuration);
+		waitForJobsToComplete();
+
+		project1.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+		project1.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+		waitForJobsToComplete();
+
+		assertNoErrors(project1);
+
+		IFile interfaceFile = project1
+				.getFile("src/main/java/org/objectledge/maven/connectors/coral/tests/SampleResource.java");
+
+		assertTrue("interface file not synchronized", interfaceFile.isSynchronized(IResource.DEPTH_ZERO));
+		assertTrue("interface file not accessible", interfaceFile.isAccessible());
+	}
+	
+	
+	public void testIncrementalBuild() throws Exception {
+		ResolverConfiguration configuration = new ResolverConfiguration();
+		IProject project1 = importProject("projects/testproject-02/pom.xml",
+				configuration);
+		waitForJobsToComplete();
+
+		project1.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+		project1.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+		waitForJobsToComplete();
+
+		assertNoErrors(project1);
+
+		IFile interfaceFile = project1
+				.getFile("src/main/java/org/objectledge/maven/connectors/coral/tests/SampleResource.java");
+
+		assertTrue("interface file not synchronized", interfaceFile.isSynchronized(IResource.DEPTH_ZERO));
+		assertTrue("interface file not accessible", interfaceFile.isAccessible());
+
+		IFile rmlFile = project1.getFile("src/main/resources/rml/sample.rml");
+		String rmlContents = getContents(rmlFile);
+		String interfaceContents = getContents(interfaceFile);
+		assertFalse("interface file not clean", interfaceContents.contains("getSecondAttribute()"));
+
+		rmlContents = rmlContents.replace("string firstProperty",
+				"string firstProperty,\nstring secondAttribute");
+		setContents(rmlFile, rmlContents);
+		project1.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+		waitForJobsToComplete();
+
+		interfaceFile = project1
+				.getFile("src/main/java/org/objectledge/maven/connectors/coral/tests/SampleResource.java");
+		assertTrue("interface file not synchronized", interfaceFile.isSynchronized(IResource.DEPTH_ZERO));
+		interfaceContents = getContents(interfaceFile);
+		assertTrue("interface file does not reference secondAttribute", interfaceContents.contains("getSecondAttribute()"));
+	}
+	
+
+	private void setContents(IFile file, String contents) throws CoreException,
+			UnsupportedEncodingException {
+		file.setContents(new ByteArrayInputStream(contents.getBytes("UTF-8")),
+				IResource.FORCE, monitor);
+	}
+
+	protected String getContents(IFile file) throws IOException, CoreException {
+		return FileUtils.fileRead(file.getLocation().toFile(),
+				file.getCharset());
 	}
 }
