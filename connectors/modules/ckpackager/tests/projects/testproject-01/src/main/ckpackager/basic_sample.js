f6ad6cf566ed813eec868d367676b0d116e55923
rkrzewski <rafal.krzewski@objectledge.org>
2011-07-07 16:46:06 +0200

    TOOLS-38: added license information

--- connectors/modules/ckpackager/tests/projects/testproject-01/src/main/ckpackager/basic_sample.js
+++ connectors/modules/ckpackager/tests/projects/testproject-01/src/main/ckpackager/basic_sample.js
@@ -1,4 +1,15 @@
 /*
+ * Copyright (c) 2011 Caltha - Krzewski, Mach, Potempski Sp. J.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Caltha - Krzewski, Mach, Potempski Sp. J.
+ */
+/*
  * #########
  *  This is a sample script file, were we can check all features of FCKpackager.
  *  The code has not much sense and it's here for demonstration purposes only.
--- connectors/modules/jflex/plugin/src/org/objectledge/maven/connectors/jflex/JFlexBuildParticipant.java
+++ connectors/modules/jflex/plugin/src/org/objectledge/maven/connectors/jflex/JFlexBuildParticipant.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2011 Caltha - Krzewski, Mach, Potempski Sp. J.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Caltha - Krzewski, Mach, Potempski Sp. J.
+ */
 package org.objectledge.maven.connectors.jflex;
 
 import java.io.File;
