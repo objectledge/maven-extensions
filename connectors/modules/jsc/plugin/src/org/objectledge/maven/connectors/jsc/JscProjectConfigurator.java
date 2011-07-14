b91560f3780765af7122a9e18ee18037e8e936cc
rkrzewski <rafal.krzewski@objectledge.org>
2011-07-15 01:05:48 +0200

    TOOLS-41: create error markeres for non-existent source directories

--- connectors/modules/jsc/plugin/src/org/objectledge/maven/connectors/jsc/JscProjectConfigurator.java
+++ connectors/modules/jsc/plugin/src/org/objectledge/maven/connectors/jsc/JscProjectConfigurator.java
@@ -12,22 +12,35 @@
 package org.objectledge.maven.connectors.jsc;
 
 import java.io.File;
+import java.util.List;
 
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.m2e.core.internal.markers.MavenProblemInfo;
+import org.eclipse.m2e.core.internal.markers.SourceLocation;
+import org.eclipse.m2e.core.internal.markers.SourceLocationHelper;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.MojoExecutionKey;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
 import org.eclipse.m2e.jdt.IClasspathDescriptor;
 
 public class JscProjectConfigurator extends AbstractJavaProjectConfigurator {
+
+	public static String MARKER_ID = "org.objectledge.maven.connectors.jsc.";
+
+	public static String MARKER_SOURCE_DIR_MISSING = MARKER_ID
+			+ "sourceDirMissing";
+
 	@Override
 	public AbstractBuildParticipant getBuildParticipant(
 			IMavenProjectFacade projectFacade, MojoExecution execution,
@@ -35,14 +48,36 @@ public class JscProjectConfigurator extends AbstractJavaProjectConfigurator {
 		return new JscBuildParticipant(execution);
 	}
 
+	@SuppressWarnings("restriction")
 	@Override
 	public void configureRawClasspath(ProjectConfigurationRequest request,
 			IClasspathDescriptor classpath, IProgressMonitor monitor)
 			throws CoreException {
 
 		IMavenProjectFacade facade = request.getMavenProjectFacade();
+		IFile pom = facade.getPom();
 
 		assertHasNature(request.getProject(), JavaCore.NATURE_ID);
+
+		markerManager.deleteMarkers(pom, MARKER_ID);
+
+		for (MojoExecution mojoExecution : getMojoExecutions(request, monitor)) {
+			// TODO check if all plugin dependencies are available
+			
+			File sourceDirectory = getParameterValue("sourceDirectory",
+					File.class, request.getMavenSession(), mojoExecution);
+			if (!sourceDirectory.exists() || !sourceDirectory.isDirectory()) {
+				SourceLocation location = SourceLocationHelper.findLocation(
+						facade.getMavenProject(), new MojoExecutionKey(
+								mojoExecution));
+				markerManager
+						.addErrorMarker(pom, MARKER_SOURCE_DIR_MISSING,
+								new MavenProblemInfo("sourceDirectory "
+										+ sourceDirectory + " does not exist",
+										location));
+
+			}
+		}
 
 		for (MojoExecution mojoExecution : getMojoExecutions(request, monitor)) {
 
